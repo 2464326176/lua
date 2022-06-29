6b8fffd6456cf1bcd287a7fc7eb9cebb985975df
刘宇航 <2464326176@qq.com>
2022-06-29 22:52:16 +0800

    lua

---
+++ config/config.lua
@@ -0,0 +1,13 @@
+/*
+ * @Author: yh
+ * @Date: 2022/6/28 22:28
+ * @Description: 
+ * @FilePath: config.lua.cpp
+ */
+
+mystr = "hello world"
+my_table = {name = "yxli8", id = 123456}
+
+function a_add_b(a,b)
+    return a+b
+end
